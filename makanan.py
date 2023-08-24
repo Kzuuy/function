ca67b5f42fcb57470bb32796057d56555a065aa5
Zuuya <122768250+Kzuuy@users.noreply.github.com>
2023-08-24 13:15:47 +0700

    Add files via upload

---
+++ makanan.py
@@ -0,0 +1,5 @@
+makanan = ("nasi tutug oncom", "sate", "baso", "seblak", "indomie" )
+update = list(makanan)
+update[0] = "nasi timbel"
+tuple = tuple(update)
+print(tuple)
---
+++ minuman.py
@@ -0,0 +1,9 @@
+minuman = ["air zamzam", "fresh tea", "coca coola", "pepsi", "teh kotak"]
+print(minuman[2])
+
+#append itu menambahkan data dari belakang
+minuman.append("marimas")
+
+#pop itu menghapus data
+minuman.pop(2)
+print(minuman)
